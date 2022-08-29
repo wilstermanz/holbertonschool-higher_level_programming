a5955d3af6eefee53848ed55a119d625266ed6e5
wilstermanz <wilstermanz@gmail.com>
2022-08-29 15:03:53 -0500

    Solution for task 8

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+flag = 0
+for i in range(0, 10):
+    for j in range(0, 10):
+        if j > i:
+            if flag == 1:
+                print(", ", end="")
+            print("{:d}{:d}".format(i, j), end="")
+            flag = 1
+print("")
