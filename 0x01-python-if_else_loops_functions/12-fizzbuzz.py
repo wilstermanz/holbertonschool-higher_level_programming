8bf23253304f559b346bf82c8d3cce650f297992
wilstermanz <wilstermanz@gmail.com>
2022-08-29 19:53:58 -0500

    Solution for task 14

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    flag = 0
+    for i in range(1, 100):
+        if flag == 1:
+            print(" ", end='')
+        if i % 3 == 0:
+            print("Fizz", end='')
+        if i % 5 == 0:
+            print("Buzz", end='')
+        if i % 3 != 0 and i % 5 != 0:
+            print(i, end='')
+        flag = 1
+    print("")
