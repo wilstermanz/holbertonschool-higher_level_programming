f8a348b506405e36036ebb8484d1fffb8fe0e439
wilstermanz <wilstermanz@gmail.com>
2022-08-29 19:59:03 -0500

    Added space at end

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    flag = 0
     for i in range(1, 101):
-        if flag == 1:
-            print(" ", end='')
         if i % 3 == 0:
             print("Fizz", end='')
         if i % 5 == 0:
             print("Buzz", end='')
         if i % 3 != 0 and i % 5 != 0:
             print(i, end='')
-        flag = 1
+        print(" ", end='')
