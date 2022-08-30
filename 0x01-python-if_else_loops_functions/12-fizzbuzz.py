f68fc0c21dcf0bf5d059158ecc6252e021f2f526
wilstermanz <wilstermanz@gmail.com>
2022-08-29 19:56:17 -0500

    Fixed data range

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     flag = 0
-    for i in range(1, 100):
+    for i in range(1, 101):
         if flag == 1:
             print(" ", end='')
         if i % 3 == 0:
