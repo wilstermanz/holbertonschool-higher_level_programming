af58cb98b5c2073cbf4f912dcb4fcfbf80239eee
wilstermanz <wilstermanz@gmail.com>
2022-08-31 12:32:53 -0500

    Solution for task 1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+if len(argv) == 2:
+    print("1 argument:")
+if len(argv) > 2:
+    print("{} arguments:".format(len(argv) - 1))
+for i in range(1, len(argv)):
+    print("{}: {}".format(i, argv[i]))
