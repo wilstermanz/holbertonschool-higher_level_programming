bea3e197674984794493eb61afc82ca2a59d8148
wilstermanz <wilstermanz@gmail.com>
2022-08-31 13:12:46 -0500

    Removed auto-added import line

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-from decimal import DivisionByZero
-
-
 def safe_print_division(a, b):
     try:
         product = a / b
-    except:
+    except ZeroDivisionError:
         product = "None"
     finally:
         print("Inside result: {}".format(product))
