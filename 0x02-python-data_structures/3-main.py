defd859cc554c553999592aa47e2aa5f07bd0752
wilstermanz <wilstermanz@gmail.com>
2022-09-01 12:19:20 -0500

    Added if check

--- 0x02-python-data_structures/3-main.py
+++ 0x02-python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [0, 0, -0, 1]
+my_list = [0, 0, -0, 1, 0, 0, -0, 1, 0, 0, -0, 1, 0, 0, -0, 1, 0, 0, -0, 1, 0, 0, -0, 1]
 print_reversed_list_integer(my_list)
