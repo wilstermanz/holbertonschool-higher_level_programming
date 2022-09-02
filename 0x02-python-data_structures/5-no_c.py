0095425accfd2c01ae5a0f149836cea6898dc515
wilstermanz <wilstermanz@gmail.com>
2022-09-02 10:44:07 -0500

    Python is weird

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string[:]:
-        if i != 'c' and i != 'C':
-            new_string += i
-    return new_string
+    return my_string.translate({ord('c'): None, ord('C'): None})
