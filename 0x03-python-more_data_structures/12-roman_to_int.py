dd2631e34eaceba481c9537ed738ee28c8bcc67d
wilstermanz <wilstermanz@gmail.com>
2022-09-06 17:27:49 -0500

    Added type check

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or type(roman_string) != str:
         return 0
     sum = 0
     r_to_i = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
