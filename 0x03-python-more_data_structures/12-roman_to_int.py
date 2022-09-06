a0a6962abb1e4451594d5c68de34995f30b59358
wilstermanz <wilstermanz@gmail.com>
2022-09-06 17:25:50 -0500

    Solution for task 12

---
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    sum = 0
+    r_to_i = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for i in range(0, len(roman_string)):
+        if i > 0 and r_to_i[roman_string[i]] > r_to_i[roman_string[i-1]]:
+            sum -= r_to_i[roman_string[i-1]]
+            sum += r_to_i[roman_string[i]] - r_to_i[roman_string[i-1]]
+        else:
+            sum += r_to_i[roman_string[i]]
+
+    return sum
