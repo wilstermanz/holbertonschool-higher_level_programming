d78773f94a6f34c38190b0ceea5d2c6e882c1740
wilstermanz <wilstermanz@gmail.com>
2022-09-07 11:33:45 -0500

    Solution for task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Contains info relating to square classes"""
+
+
+class Square:
+    """Creates a square and defines the length of a side"""
+
+    def __init__(self, size=0):
+        """Initializes square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
