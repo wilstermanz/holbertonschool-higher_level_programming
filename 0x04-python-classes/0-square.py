c63babd24ee64c08df705df26e7d138c2ea18334
wilstermanz <wilstermanz@gmail.com>
2022-09-07 10:59:15 -0500

    Added documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This is the docstring for module"""
 class Square:
+    """This class creates a square and defines the length of a side"""
     def __init__(self, size):
+        """Initializes square and sets size"""
         self.__size = size
