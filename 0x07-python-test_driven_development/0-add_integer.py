840814acfbd30c46093af8e2ca7a4d709eff5a34
wilstermanz <wilstermanz@gmail.com>
2022-09-12 16:03:14 -0500

    test 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""
-    Task 0
-    Write a function that adds 2 integers.
-    Prototype: def add_integer(a, b=98):
+"""Returns the sum of two integers, or floats typecasted as int
 """
 
 
 def add_integer(a, b=98):
-    """
-        Adds two integers and returns the sum
-    """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
+    """Math takes place here"""
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
 
-    return a + b
+    """
+    if type(a) is complex:
+        raise TypeError("a must be an integer")
+    if type(b) is complex:
+        raise TypeError("b must be an integer")
+    """
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return(a + b)
