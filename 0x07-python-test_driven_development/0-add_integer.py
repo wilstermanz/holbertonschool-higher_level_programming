57bf9d9d572865b34d586aa9beb2835b709f260b
wilstermanz <wilstermanz@gmail.com>
2022-09-12 16:22:29 -0500

    rollback

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,16 +6,17 @@
 """
 
 
-from types import NoneType
-
-
 def add_integer(a, b=98):
     """
         Adds two integers and returns the sum
     """
-    if type(a) not in [int, float, NoneType]:
+    if a is None:
+        raise TypeError("'NoneType' object is not subscriptable")
+    if b is None:
+        raise TypeError("'NoneType' object is not subscriptable")
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float, NoneType]:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
