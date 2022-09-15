9361a1db2035d466c2c9881565f1a01e8afa42fd
wilstermanz <wilstermanz@gmail.com>
2022-09-15 13:21:59 -0500

    Added docstrings to functions

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,13 @@ class BaseGeometry:
     """Defines something"""
 
     def area(self):
+        """This doesn't do anything"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Checks for valid integers"""
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
