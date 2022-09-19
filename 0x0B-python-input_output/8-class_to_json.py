1ca37878ebf7f11e0b9d2d8f1014e436c1a7a1d8
wilstermanz <wilstermanz@gmail.com>
2022-09-19 14:35:36 -0500

    Solution for task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the required functions for task 8"""
+
+
+def class_to_json(obj):
+    """Returns library of variables and values in obj"""
+    return vars(obj)
