f1be059ee69a72145d9a64b007f91b435d5dd3aa
wilstermanz <wilstermanz@gmail.com>
2022-09-19 13:29:57 -0500

    Solution for task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains required functions for task 3"""
+import json
+
+
+def to_json_string(my_obj):
+    """This module converts an object as its JSON representation"""
+    return json.dumps(my_obj)
