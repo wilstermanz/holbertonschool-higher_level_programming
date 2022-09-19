539794e20f49a92659287992d22a43ac9920ab1d
wilstermanz <wilstermanz@gmail.com>
2022-09-19 15:39:09 -0500

    Added docstring to function

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,6 +3,10 @@
 
 
 def pascal_triangle(n):
+    """
+        This function creates a list of lists representing
+        pascal's triangle for a given number of rows
+    """
     if n <= 0:
         return []
 
