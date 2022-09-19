68e92431d81771bfa9e2ebc3cbc224995c181f5f
wilstermanz <wilstermanz@gmail.com>
2022-09-19 13:08:15 -0500

    Added docstring to read_file function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """This function opens and prints a file to stdout"""
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end='')
