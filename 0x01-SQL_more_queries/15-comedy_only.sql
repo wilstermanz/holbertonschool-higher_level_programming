96faf5a38a944c31a90a53872ca8761da98d59b6
wilstermanz <wilstermanz@gmail.com>
2022-10-06 20:42:03 -0500

    capitalized keyword

--- 0x01-SQL_more_queries/15-comedy_only.sql
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title AS title
-from tv_shows
+FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
