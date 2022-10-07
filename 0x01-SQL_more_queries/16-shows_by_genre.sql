2b2132da3e178c5967f844166979a1dc629989a6
wilstermanz <wilstermanz@gmail.com>
2022-10-06 20:51:04 -0500

    CAPITALIZED KEYWORD

--- 0x01-SQL_more_queries/16-shows_by_genre.sql
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_shows.title AS title, tv_genres.name as name
+SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
