7fffec1c45c100352c253013cb52bf9c8bfa6fe5
wilstermanz <wilstermanz@gmail.com>
2022-10-19 19:53:57 -0500

    Solution for task 2

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module contains a script that will list states in a MySQL database"""
+import MySQLdb
+from sys import argv
+
+
+def filter_my_states():
+    """
+    Takes in an argument and displays all values in
+    the states table where name matches the argument.
+    """
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3]
+                         )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states "
+                f"WHERE name = '{argv[4]}' "
+                "ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_my_states()
