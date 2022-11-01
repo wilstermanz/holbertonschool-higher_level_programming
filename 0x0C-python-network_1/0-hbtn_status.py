7a5f01461f491cbc2c4ca676210ef4f8e2dd44d3
wilstermanz <wilstermanz@gmail.com>
2022-11-01 18:34:28 -0500

    Formating

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,10 +6,11 @@ from urllib import request
 def main():
     """Main function"""
     with request.urlopen("https://intranet.hbtn.io/status") as resp:
-        print(f"Body response:\n\
-                \t- type: {type(resp.peek())}\n\
-                \t- content: {resp.peek()}\n\
-                \t- utf8 content: {resp.peek().decode('utf-8')}")
+        print(f"Body response:\n"
+              f"\t- type: {type(resp.peek())}\n"
+              f"\t- content: {resp.peek()}\n"
+              f"\t- utf8 content: {resp.peek().decode('utf-8')}\n"
+              )
 
 
 if __name__ == "__main__":
