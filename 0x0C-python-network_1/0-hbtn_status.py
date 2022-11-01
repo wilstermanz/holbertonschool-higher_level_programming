1e89d7c605268f2211d78ba5bcbe60a5fafa8cb4
wilstermanz <wilstermanz@gmail.com>
2022-11-01 18:35:49 -0500

    again

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,15 +3,10 @@
 from urllib import request
 
 
-def main():
-    """Main function"""
-    with request.urlopen("https://intranet.hbtn.io/status") as resp:
-        print(f"Body response:\n"
-              f"\t- type: {type(resp.peek())}\n"
-              f"\t- content: {resp.peek()}\n"
-              f"\t- utf8 content: {resp.peek().decode('utf-8')}\n"
-              )
-
-
 if __name__ == "__main__":
-    main()
+    with request.urlopen("https://intranet.hbtn.io/status") as resp:
+            print(f"Body response:\n"
+                f"\t- type: {type(resp.peek())}\n"
+                f"\t- content: {resp.peek()}\n"
+                f"\t- utf8 content: {resp.peek().decode('utf-8')}\n"
+                )
