602654f8b243bd970bfd07a569ac3994d44de354
wilstermanz <wilstermanz@gmail.com>
2022-11-01 18:51:37 -0500

    changed import

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-from urllib import request
-
+import urllib
 
 if __name__ == "__main__":
     """import guarded"""
-    with request.urlopen("https://intranet.hbtn.io/status") as resp:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
         print(f"Body response:\n"
               f"\t- type: {type(resp.peek())}\n"
               f"\t- content: {resp.peek()}\n"
