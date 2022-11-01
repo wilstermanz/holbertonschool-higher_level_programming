1e54f15eb120857292e56352e7650a2d45535955
wilstermanz <wilstermanz@gmail.com>
2022-11-01 18:49:45 -0500

    formatting and docstring

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@ from urllib import request
 
 
 if __name__ == "__main__":
+    """import guarded"""
     with request.urlopen("https://intranet.hbtn.io/status") as resp:
-            print(f"Body response:\n"
-                f"\t- type: {type(resp.peek())}\n"
-                f"\t- content: {resp.peek()}\n"
-                f"\t- utf8 content: {resp.peek().decode('utf-8')}\n"
-                )
+        print(f"Body response:\n"
+              f"\t- type: {type(resp.peek())}\n"
+              f"\t- content: {resp.peek()}\n"
+              f"\t- utf8 content: {resp.peek().decode('utf-8')}\n"
+              )
