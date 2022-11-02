294eae679b8885e0a852fa112f495aa6f0b34bcd
wilstermanz <wilstermanz@gmail.com>
2022-11-01 22:19:23 -0500

    checks checks checks

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -7,17 +7,17 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    if argv == 1:
-        q = ''
-    else:
-        q = argv[1]
+def main(q=""):
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
         r_dict = r.json()
-        if len(r_dict) == 0:
+        if r_dict.get('id') is None:
             print("No Result")
         else:
             print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
     except Exception:
         print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main(argv[1] if len(argv) != 1 else main())
