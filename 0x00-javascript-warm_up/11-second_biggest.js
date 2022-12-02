7fef77902ee8d6f1eaf9fea9b9fc9d129c97581d
wilstermanz <wilstermanz@gmail.com>
2022-12-01 20:59:55 -0600

    Parses ints

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -5,11 +5,12 @@ if (process.argv.length <= 2) {
   console.log(secondBiggest);
 } else {
   for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > biggest) {
+    const current = parseInt(process.argv[i]);
+    if (current > biggest) {
       secondBiggest = biggest;
-      biggest = process.argv[i];
-    } else if (process.argv[i] > secondBiggest && process.argv[i] !== biggest) {
-      secondBiggest = process.argv[i];
+      biggest = current;
+    } else if (current > secondBiggest && current !== biggest) {
+      secondBiggest = current;
     }
   }
   console.log(secondBiggest);
