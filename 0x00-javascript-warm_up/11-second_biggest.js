acd257fb4c03c1868d3ff9afdc5c053863519765
wilstermanz <wilstermanz@gmail.com>
2022-12-01 20:48:26 -0600

    Solution for task 11

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let secondBiggest = 0;
+let biggest = 0;
+if (process.argv.length <= 2) {
+  console.log(secondBiggest);
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > biggest) {
+      secondBiggest = biggest;
+      biggest = process.argv[i];
+    } else if (process.argv[i] > secondBiggest) {
+      secondBiggest = process.argv[i];
+    }
+  }
+  console.log(secondBiggest);
+}
