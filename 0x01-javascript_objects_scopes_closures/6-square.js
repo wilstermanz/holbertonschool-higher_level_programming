de004ca1e90f0c4245c6c4ea5bbcd2e47a3eba02
wilstermanz <wilstermanz@gmail.com>
2022-12-02 19:05:02 -0600

    Now inherits from 5 instead of 4

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (!c) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
     }
