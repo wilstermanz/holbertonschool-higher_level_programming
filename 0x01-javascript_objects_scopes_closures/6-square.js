32135424ff1b2a084fd7fd5b6e1dd8d3636c3308
wilstermanz <wilstermanz@gmail.com>
2022-12-02 19:07:27 -0600

    Removed constructor

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const Square5 = require('./5-square');
 
 class Square extends Square5 {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
