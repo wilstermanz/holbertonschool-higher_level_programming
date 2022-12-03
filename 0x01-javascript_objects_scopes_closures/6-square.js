0c4ddcf377d2ca1a5583f2082f4234383227ee26
wilstermanz <wilstermanz@gmail.com>
2022-12-02 18:59:57 -0600

    Solution for task 6

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(('%c', c).repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
