594e8ec4cf99cb031d0b05b0abed6cf56527a48d
wilstermanz <wilstermanz@gmail.com>
2022-12-02 19:06:25 -0600

    5 is a square

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./5-rectangle');
+const Square5 = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Square5 {
   constructor (size) {
     super(size, size);
   }
