e0031f630ff574b30f98c2b9388ec4c0300f379e
wilstermanz <wilstermanz@gmail.com>
2022-12-05 09:20:08 -0600

    typo

--- 0x01-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x01-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let occurences = 0;
+  let occurrences = 0;
   for (let i = 0; list[i]; i++) {
     if (list[i] === searchElement) {
-      occurences++;
+      occurrences++;
     }
   }
-  return occurences;
+  return occurrences;
 };
