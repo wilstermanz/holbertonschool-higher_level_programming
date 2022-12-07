7970c3804e8bc3af2b4ffa29bf8280e69dbc4812
wilstermanz <wilstermanz@gmail.com>
2022-12-06 21:37:26 -0600

    Solution for task 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/people/18/', function (err, response, content) {
+request.get('https://swapi-api.hbtn.io/api/films/', function (err, response, content) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    console.log(JSON.parse(content).films.length);
+    const results = JSON.parse(content).results;
+    let numFilms = 0;
+    for (const film in results) {
+      for (const i in results[film].characters) {
+        if (results[film].characters[i].includes('/18/')) {
+          numFilms++;
+        }
+      }
+    }
+    console.log(numFilms);
   }
-});
+}
+);
